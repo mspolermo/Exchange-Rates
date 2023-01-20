9fdd245c91199b9dd4f3a9298601046db2cd8ac7
mspolermo <mspolermo@gmail.com>
2023-01-20 16:32:34 +0500

    Logic was started creating

---
+++ script.js
@@ -0,0 +1,21 @@
+async function getCurrencies (){
+  try {
+    const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
+    const  data = await response.json();
+
+    const usdRate = data.Valute.USD.Value.toFixed(2);
+    const eurRate = data.Valute.EUR.Value.toFixed(2);
+
+    const usdElement=document.querySelector('#usd');
+    const eurElement=document.querySelector('#eur');
+
+    usdElement.innerText=usdRate;
+    eurElement.innerText=eurRate;
+
+  }catch{
+console.log('Сервер не отвечает(')
+  }
+
+};
+
+getCurrencies ();
