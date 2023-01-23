f16a79e464dcb4c98a3174e6614cf1596e4cad41
mspolermo <mspolermo@gmail.com>
2023-01-23 21:40:09 +0500

    Version 0.4

--- script.js
+++ script.js
@@ -12,59 +12,115 @@ async function getCurrencies (){
       leftRate = data.Valute.CNY.Value.toFixed(2);
     }else if (leftCurency.innerText === GEL.innerText) {
       leftRate = data.Valute.GEL.Value.toFixed(2);
-    }
+    };
 
-    const eurRate = data.Valute.EUR.Value.toFixed(2);
+    let rightRate = data.Valute.EUR.Value.toFixed(2);
+    if (rightCurency.innerText === PLN.innerText) {
+      rightRate = data.Valute.PLN.Value.toFixed(2);
+    }else if (rightCurency.innerText === INR.innerText) {
+      rightRate = data.Valute.INR.Value.toFixed(2);
+    }else if (rightCurency.innerText === TRY.innerText) {
+      rightRate = data.Valute.TRY.Value.toFixed(2);
+    }else if (rightCurency.innerText === KRW.innerText) {
+      rightRate = data.Valute.KRW.Value.toFixed(2);
+    };
 
     const leftElement=document.querySelector('#usd');
-    const eurElement=document.querySelector('#eur');
+    const rightElement=document.querySelector('#eur');
 
     leftElement.innerText=leftRate;
-    eurElement.innerText=eurRate;
+    rightElement.innerText=rightRate;
 
   }catch{
 console.log('Сервер не отвечает(')
   }
 };
 
+
 getCurrencies ();
 
-
-
 const leftCurency= document.querySelector("#currency-info-left");
-const leftContainer = document.querySelector("#currency-container1");
+const leftContainer = document.querySelector("#currency-container-left");
 const USD = document.querySelector("#USD");
 const GBP = document.querySelector("#GBP");
 const KZT = document.querySelector("#KZT");
 const CNY = document.querySelector("#CNY");
 const GEL = document.querySelector("#GEL");
 
-leftCurency.addEventListener('click', function() {
-    leftContainer.classList.remove('none')
+function hideChangeTableLeft() {
+  leftContainer.classList.add('none');
+};
+
+leftCurency.addEventListener('click', function(){
+  leftContainer.classList.remove('none');
 });
+
 USD.addEventListener('click', function(){
-  leftContainer.classList.add('none')
+  hideChangeTableLeft();
   leftCurency.innerText = USD.innerText;
   getCurrencies ();
 });
 GBP.addEventListener('click', function(){
-  leftContainer.classList.add('none')
+  hideChangeTableLeft();
   leftCurency.innerText = GBP.innerText;
   getCurrencies ();
 });
 KZT.addEventListener('click', function(){
-  leftContainer.classList.add('none')
+  hideChangeTableLeft();
   leftCurency.innerText = KZT.innerText;
   getCurrencies ();
 });
 CNY.addEventListener('click', function(){
-  leftContainer.classList.add('none')
+  hideChangeTableLeft();
   leftCurency.innerText = CNY.innerText;
   getCurrencies ();
 });
 GEL.addEventListener('click', function(){
-  leftContainer.classList.add('none')
+  hideChangeTableLeft();
   leftCurency.innerText = GEL.innerText;
   getCurrencies ();
 });
 
+
+
+const rightCurency= document.querySelector("#currency-info-right");
+const rightContainer = document.querySelector("#currency-container-right");
+const EUR = document.querySelector("#EUR");
+const PLN = document.querySelector("#PLN");
+const INR = document.querySelector("#INR");
+const TRY = document.querySelector("#TRY");
+const KRW = document.querySelector("#KRW");
+
+function hideChangeTableRight() {
+  rightContainer.classList.add('none');
+};
+
+rightCurency.addEventListener('click', function(){
+  rightContainer.classList.remove('none');
+});
+
+EUR.addEventListener('click', function(){
+  hideChangeTableRight();
+  rightCurency.innerText = EUR.innerText;
+  getCurrencies ();
+});
+PLN.addEventListener('click', function(){
+  hideChangeTableRight();
+  rightCurency.innerText = PLN.innerText;
+  getCurrencies ();
+});
+INR.addEventListener('click', function(){
+  hideChangeTableRight();
+  rightCurency.innerText = INR.innerText;
+  getCurrencies ();
+});
+TRY.addEventListener('click', function(){
+  hideChangeTableRight();
+  rightCurency.innerText = TRY.innerText;
+  getCurrencies ();
+});
+KRW.addEventListener('click', function(){
+  hideChangeTableRight();
+  rightCurency.innerText = KRW.innerText;
+  getCurrencies ();
+});
