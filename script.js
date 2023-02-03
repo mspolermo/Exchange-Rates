7aa3c608e22c2b1045ff4339dfab4516a914648d
mspolermo <mspolermo@gmail.com>
2023-02-03 19:38:50 +0500

    Version 0.5 - rubl-padezh was added

--- script.js
+++ script.js
@@ -28,21 +28,44 @@ async function getCurrencies (){
     const leftElement=document.querySelector('#currency-value-left');
     const rightElement=document.querySelector('#currency-value-right');
 
+
+
     leftElement.innerText=leftRate;
     rightElement.innerText=rightRate;
+    
+    padezh('right', leftRate);
+    padezh('right', rightRate);
     
     document.querySelector('#currentDate').innerText=data.Date.split("").reverse().splice([15]).reverse().join("");
   }catch{
   alert('Сервер не доступен');
   }
 };
+function padezh (container, num) {
+  num=Math.floor(num);
 
+  let answer;
+  let exceptionDictionary = [11,12,13,14];
+  let arrNum = ('' + num).split('');
+  let exceptionCheck = +(arrNum[arrNum.length-2]+arrNum[arrNum.length-1]);
+
+  if (exceptionDictionary.includes(exceptionCheck)) {
+    answer='рублей'}
+  else if (num%10 == 1) {
+    answer='рубль';
+  } else if ((num%10 > 4) || (num%10 ==0)){
+    answer='рублей';
+  } else {answer='рубля'}
+
+  document.querySelector('#rub-'+container).innerText = (answer);
+  }
 getCurrencies ();
 
 const leftCurency= document.querySelector("#currency-info-left");
 const leftContainer = document.querySelector("#currency-container-left");
 const rightCurency= document.querySelector("#currency-info-right");
 const rightContainer = document.querySelector("#currency-container-right");
+const rub = document.querySelector('#rub');
 
 leftCurency.addEventListener('click', function(){
   leftContainer.classList.remove('none');
@@ -65,12 +88,16 @@ function currencyClick (currency, container) {
     getCurrencies ();
   };
   });
+    
+
   currency.addEventListener('mouseover', function(){
   document.querySelector('#'+currency.id).innerText = (currency.id);
     });
   currency.addEventListener('mouseleave', function(){
   document.querySelector('#'+currency.id).innerText = (currencySymbol);
     });
+    
+  
 };
 
 currencyClick(USD, 'left');
